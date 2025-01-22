2134e62360a1af9ceebe84c273f1396b7d425b53
summersampson <summersampson57@gmail.com>
2025-01-22 09:22:28 -0700

    Add project files.

---
+++ FoodItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class FoodItem
+{
+    // each food has a name, category, quantity, and expiration date
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public int Quantity { get; set; }
+    public DateTime ExpirationDate { get; set; }
+
+    // getting the info for each food item set up with FoodItem
+    public FoodItem(string name, string category, int quantity, DateTime expirationDate)
+    {
+        // error handling - making sure there is atleast 1 of the food in stock
+        if (quantity < 0)
+            throw new ArgumentException("Quantity must be positive.");
+
+        Name = name;
+        Category = category;
+        Quantity = quantity;
+        ExpirationDate = expirationDate;
+    }
+
+    // ToString is called in the PrintFoodItems method so the food is printed out in a nice way
+    public override string ToString()
+    // printing each individual food item with how much it has
+    {
+        return $"{Name} ({Category}) - {Quantity} units - Expires on {ExpirationDate:MM/dd/yyyy}";
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+// program.cs controls the flow of the program
+
+class Program
+{
+    static List<FoodItem> inventory = new List<FoodItem>();
+
+    // the main method doesn't return any value and it starts here
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            // all of the options at the beginning
+            Console.WriteLine("\nFood Bank Inventory System");
+            Console.WriteLine("1-Add Food Item");
+            Console.WriteLine("2-Delete Food Item");
+            Console.WriteLine("3-Print List of Current Food Items");
+            Console.WriteLine("4-Exit the Program");
+            Console.WriteLine("Choose an option:");
+
+            string choice = Console.ReadLine();
+
+            // this is just taking the user to the method they want based on the number they said
+            switch (choice)
+            {
+                case "1":
+                    AddFoodItem();
+                    break;
+                case "2":
+                    DeleteFoodItem();
+                    break;
+                case "3":
+                    PrintFoodItems();
+                    break;
+                case "4":
+                    // exit the program if they type 4
+                    Console.WriteLine("Exiting the program");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+
+    // menu to add food method
+    static void AddFoodItem()
+    {
+        // getting the info for each food item (to be sent to FoodItem.cs)
+        Console.Write("Enter the food item: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter the food item category: ");
+        string category = Console.ReadLine();
+
+        Console.Write("Enter the quantity: ");
+        int quantity = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the expiration date (MM/DD/YYYY): ");
+        DateTime expirationDate = DateTime.Parse(Console.ReadLine());
+
+        // this line is where I'm creating a new object using the FoodItem constructor
+        FoodItem newItem = new FoodItem(name, category, quantity, expirationDate);
+        
+        // food item stores info for individual food with a list (called inventory)
+        inventory.Add(newItem);
+
+        Console.WriteLine("Food item added :)");
+    }
+
+    // delete food method
+    static void DeleteFoodItem()
+    {
+        Console.WriteLine("Enter the food you want to delete: ");
+        string name = Console.ReadLine();
+
+        // this allows you to search for food ignoring upper and lower case
+        FoodItem itemToRemove = inventory.Find(item => item.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        if (itemToRemove != null)
+        {
+            // deleting from the list (inventory is the list of food items)
+            inventory.Remove(itemToRemove);
+            Console.WriteLine("Item deleted!");
+        }
+        // telling you if it can't find the item
+        else
+        {
+            Console.WriteLine("Item not found");
+        }
+    }
+
+    // print list of current food options method
+    static void PrintFoodItems()
+    {
+        // error handling - if there is not inventory tell the user
+        if (inventory.Count == 0)
+        {
+            Console.WriteLine("No food items in inventory");
+        }
+        else
+        {
+            Console.WriteLine("Current food items: ");
+            foreach (var item in inventory)
+            {
+                // ToString prints out the item in the string set up on FoodItem.cs
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
+
